7cb610f0db6fb2f1a789bf77f28db625e271508b
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-01 14:47:05 -0500

    experimenting with forms, submit, prevent form, and keyup

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
     addCounter() {this.counter = this.counter + 1},
     removeCounter() {this.counter = this.counter -1},
     setName(event, lastName) { this.name = event.target.value + ` ${lastName}`},
+    submitForm() {
+      alert('Submitted!')
+    },
+    confirmInput() {
+      this.name = 'Just another Name'
+    }
   }
 });
 
