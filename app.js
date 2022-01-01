12460d314cff1280e0ac544e0362103b2ba5b8f3
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-01 14:35:22 -0500

    init and passing event down with another arg into method

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+  data: () => ({
+    counter: 0,
+    name: '',
+  }),
+  methods: {
+    addCounter() {this.counter = this.counter + 1},
+    removeCounter() {this.counter = this.counter -1},
+    setName(event, lastName) { this.name = event.target.value + ` ${lastName}`},
+  }
+});
+
+app.mount('#events');
