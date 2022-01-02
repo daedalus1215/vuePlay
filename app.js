3b8b502d911c7057ea86e32c55a8179aaa432009
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-01 20:44:10 -0500

    computed property

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ const app = Vue.createApp({
     counter: 0,
     name: '',
   }),
-  methods: {
-    addCounter() {this.counter = this.counter + 1},
-    removeCounter() {this.counter = this.counter -1},
-    setName(event, lastName) { this.name = event.target.value + ` ${lastName}`},
-    submitForm() {
-      alert('Submitted!')
+  computed: {
+    fullname() {
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return `${this.name} Schwarzmuller`
     },
-    confirmInput() {
-      this.name = 'Just another Name'
-    },
-    resetInput() {
-      
+    methods: {
+      addCounter() { this.counter = this.counter + 1 },
+      removeCounter() { this.counter = this.counter - 1 },
+      setName(event, lastName) { this.name = event.target.value + ` ${lastName}` },
+      submitForm() {
+        alert('Submitted!')
+      },
+      confirmInput() {
+        this.name = 'Just another Name'
+      },
     }
   }
 });
