271543b01387f5545e3ad166757de319a62b83a3
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-03 17:04:54 -0500

    Provide and inject variables (topics)

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,122 @@
 <template>
-  <h2>My Friends</h2>
-  <friend-contact
-    v-for="friend in friends"
-    :key="friend.name"
-    v-bind="friend"
-    @toggle-favorite="toggleFavoriteStatus"
-     @delete-friend="deleteFriend" 
-  />
-  <new-friend @add-contact="addContact"/>
+  <div>
+    <active-element
+      :topic-title="activeTopic && activeTopic.title"
+      :text="activeTopic && activeTopic.fullText"
+    ></active-element>
+    <knowledge-base
+      :topics="topics"
+      @select-topic="activateTopic"
+    ></knowledge-base>
+  </div>
 </template>
 
 <script>
-import FriendContact from "./components/FriendContact.vue";
-import NewFriend from "./components/NewFriend.vue";
-
 export default {
-  components: { FriendContact, NewFriend },
-  name: "App",
   data() {
     return {
-      friends: [
+      topics: [
         {
-          name: "Manual2",
-          email: "fiction@d.com2",
-          isFavorite: true,
+          id: 'basics',
+          title: 'The Basics',
+          description: 'Core Vue basics you have to know',
+          fullText:
+            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
         },
         {
-          name: "Lisa",
-          email: "lisa@d.com2",
-          isFavorite: false,
+          id: 'components',
+          title: 'Components',
+          description:
+            'Components are a core concept for building Vue UIs and apps',
+          fullText:
+            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
         },
       ],
+      activeTopic: null,
+    };
+  },
+  provide() {
+    return {
+      topics: this.topics,
     };
   },
   methods: {
-    toggleFavoriteStatus(name) {
-      const identifiedFriend = this.friends.find(
-        (friend) => friend.name === name
-      );
-      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    activateTopic(topicId) {
+      console.log('activate topics', topicId);
+      this.activeTopic = this.topics.find((topic) => topic.id === topicId);
+      console.log('activeTopic now', this.activeTopic)
     },
-    addContact(name, phone, email) {
-      const newFriendContact = {
-        id: new Date().toISOString(),
-        name: name,
-        phone: phone,
-        email: email,
-        isFavorite: false,
-      };
-
-      this.friends.push(newFriendContact);
-    },
-    deleteFriend(name) {
-      console.log('app, friend deleted')
-      this.friends = this.friends.filter(friend => friend.name !== name);
-    },
+  },
+  mounted() {
+    setTimeout(
+      () =>
+        this.topics.push({
+          id: 'events',
+          title: 'Events',
+          description: 'Events are important in vue',
+          fullText: 'Events allow you to trigger code on demand!',
+        }),
+      3000
+    );
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 0;
+}
+section {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 2rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  border-radius: 12px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+li {
+  border-radius: 12px;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  width: 15rem;
+  margin: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h2 {
+  margin: 0.75rem 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #c70053;
+  background-color: #c70053;
+  color: white;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #e24d8b;
+  border-color: #e24d8b;
 }
 </style>
