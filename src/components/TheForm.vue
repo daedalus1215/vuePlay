981220286965b10effea55a71a46cc164dee6cde
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-04 15:27:02 -0500

    setting default for select

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -6,11 +6,11 @@
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model.number="userAge"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -57,12 +57,18 @@ export default {
   data() {
     return {
       userName: '',
+      userAge: null,
+      referrer: 'wom'
     }
   },
   methods: {
     submitForm() {
       console.log('Username: ' + this.userName);
+      console.log('UserAge: ' + this.userAge);
+      console.log('referrer: ' + this.referrer);
       this.userName = '';
+      this.userAge = null;
+      this.referrer = 'wom'
     }
   }
 }
