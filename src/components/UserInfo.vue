4d655b1d5a2ab5b2c326002462d00bbd6f8230d2
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-04 10:28:49 -0500

    Add slots to wrap dynamic html

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
-    <div>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <div>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </div>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
 import BadgeList from './components/BadgeList.vue';
+import BaseCard from './components/BaseCard.vue';
 
 const app = createApp(App);
 
 app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
 app.component('badge-list', BadgeList);
 
 app.mount('#app');
