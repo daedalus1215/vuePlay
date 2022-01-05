2db8eb7fd08d7f816068b13cda48123044d884ff
lala <13687739+ladams1776@users.noreply.github.com>
2022-01-05 14:24:06 -0500

    had param be a prop instead of pulling from

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to Team 2</router-link>
   </section>
 </template>
 
@@ -17,6 +18,7 @@ import UserItem from "../users/UserItem.vue";
 
 export default {
   inject: ["users", "teams"],
+  props: ['teamId'],
   components: {
     UserItem,
   },
@@ -29,13 +31,25 @@ export default {
       ],
     };
   },
+  methods: {
+    loadTeamMembers(teamId) {
+      console.log('teamId', teamId)
+      const selectedTeam = this.teams.find((team) => team.id === teamId);
+      console.log('selectedTeam', selectedTeam)
+      this.members = selectedTeam.members.map((member) => {
+        return this.users.find((user) => user.id === member);
+      });
+      this.teamName = selectedTeam.name;
+    },
+  },
   created() {
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find((team) => team.id === teamId);
-    this.members = selectedTeam.members.map(member => {
-      return this.users.find((user) => user.id === member);
-    });
-    this.teamName = selectedTeam.name;
+    this.loadTeamMembers(this.teamId);
+  },
+  watch: {
+    teamId(newId) {
+      console.log('newId', newId)
+      this.loadTeamMembers(newId);
+    },
   },
 };
 </script>
