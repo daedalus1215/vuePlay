00fce50b7ef1332848e83018a0f0cdf31852ff36
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-06 15:58:19 -0500

    routing has been refactored

---
+++ src/router.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import TeamsList from './pages/TeamsList.vue';
+import UsersList from './pages/UsersList.vue';
+import TeamMembers from './pages/TeamMembers.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', redirect: '/teams' },
+        {
+            name: 'teams',
+            path: '/teams',
+            meta: { needsAuth: true },
+            components: { default: TeamsList, footer: TeamsFooter },
+            children: [
+                {
+                    name: 'team-members',
+                    path: ':teamId',
+                    component: TeamMembers,
+                    props: true
+                },
+            ]
+        },
+        {
+            name: 'users',
+            path: '/users',
+            components: { default: UsersList, footer: UsersFooter },
+        },
+        { path: '/:notFound(.*)', component: NotFound },
+    ],
+    scrollBehavior(_, _2, savedPosition) {
+        return savedPosition && savedPosition || { left: 0, top: 0 }
+    },
+});
+
+
+router.beforeEach((to, from, next) => {
+    // sending analytics data
+    console.log(to, from);
+    console.log('Global beforeEach');
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!!')
+        next();
+    }
+});
+
+router.afterEach((to, from) => {
+    // sending analytics data
+    console.log(to, from);
+    console.log('Global aftereach');
+});
+
+
+export default router;
