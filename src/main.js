09d53f7f95c1986aef2fa4e43dae9d676f6790de
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-06 15:38:06 -0500

    POC: adding auth in middle with meta

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, nextTick } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
@@ -15,7 +15,7 @@ const router = createRouter({
         {
             name: 'teams',
             path: '/teams',
-
+            meta: { needsAuth: true },
             components: { default: TeamsList, footer: TeamsFooter },
             children: [
                 {
@@ -26,16 +26,28 @@ const router = createRouter({
                 },
             ]
         },
-        { name: 'users', 
-        path: '/users', 
-        components: { default: UsersList, footer: UsersFooter }, 
-    },
+        {
+            name: 'users',
+            path: '/users',
+            components: { default: UsersList, footer: UsersFooter },
+        },
         { path: '/:notFound(.*)', component: NotFound },
     ],
     scrollBehavior(_, _2, savedPosition) {
         return savedPosition && savedPosition || { left: 0, top: 0 }
     },
-}); 
+});
+
+
+router.beforeEach((to, from, next) => {
+    // sending analytics data
+    console.log(to, from);
+    console.log('Global beforeEach');
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!!')
+        next();
+    }
+});
 
 router.afterEach((to, from) => {
     // sending analytics data
