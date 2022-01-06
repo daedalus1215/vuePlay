3e73012a602fcf95757a6b1757bc44c43549e500
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-06 14:10:14 -0500

    confirm navigation on UserList [navigation guard]

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,22 +1,52 @@
 <template>
   <ul>
-  <button @click="confirmInput">Confirm</button>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <button @click="confirmInput">Confirm</button>
+    <button @click="saveChanges">Save Changes</button>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
 <script>
-import UserItem from './UserItem.vue';
+import UserItem from "./UserItem.vue";
 
 export default {
   components: {
     UserItem,
   },
-  inject: ['users'],
+  inject: ["users"],
   methods: {
     confirmInput() {
-      this.$router.push('/teams');
-    }
+      this.$router.push("/teams");
+    },
+  },
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
+  saveChanges() {
+    this.saveChanges = true;
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log("UserList Cmp beforeRouteEnter");
+    console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log("UserList Cmp beforeRouteLeave");
+    console.log(to, from);
+    // Can make sure that we want to leave here
+    this.changesSaved && next();
+    !this.changesSaved &&
+      next(confirm("Are you sure? You got unsaved changes!"));
+  },
+  unmounted() {
+    console.log("unmounted");
   },
 };
 </script>
