4e742b932df32cdc19a8219c831f2823107397fc
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-07 16:29:51 -0500

    setting up for vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,21 +2,13 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authStore from './modules/auth/authStore.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestsModule
-  },
-  state() {
-    return {
-      userId: 'c3'
-    };
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    }
+    requests: requestsModule,
+    auth: authStore,
   }
 });
 
