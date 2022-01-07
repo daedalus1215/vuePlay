0803fa22a407e67cec427f30f6883cb324d99db5
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-07 09:50:34 -0500

    add mapGetters and mapActions

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,14 +1,14 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <button @click="increment">Add 1</button>
+  <button @click="increase({value: 10})">Add 10</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   methods: {
-    addOne() {
-      //   this.$store.commit("increase", { value: 10 });
-      this.$store.commit({ type: "increase", value: 10 });
-    },
+      ...mapActions(['increment', 'increase'])
   },
 };
 </script>
