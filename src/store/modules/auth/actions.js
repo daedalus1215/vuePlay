a28f21d513ff3109a8eccf230e77497217265449
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-07 16:44:29 -0500

    setting up store, mutations, actions, getters, for authentication

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,26 @@
-export default {};
+import config from '../../../config';
+
+export default {
+    login() { },
+    async signup(context, payload) {
+        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${config.API}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                ...{ payload },
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log('responsData', responseData);
+            const error = new Error(responseData.message || 'Failed to authenticate');
+            throw error;
+        }
+
+        console.log('responseData', responseData)
+        context.commit('setUser', { token: responseData.idToken, userId: responseData.localId, tokenExpiration: responseData.expiresIn })
+    },
+
+};
