e1fd005ed97d09329854b738e2142916f81c6300
lala <13687739+daedalus1215@users.noreply.github.com>
2022-01-07 09:51:39 -0500

    giving alias to mapAction

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,14 +1,14 @@
 <template>
-  <button @click="increment">Add 1</button>
-  <button @click="increase({value: 10})">Add 10</button>
+  <button @click="inc">Add 1</button>
+  <button @click="increase({ value: 10 })">Add 10</button>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
   methods: {
-      ...mapActions(['increment', 'increase'])
+    ...mapActions({ inc: "increment", increase: "increase" }),
   },
 };
 </script>
